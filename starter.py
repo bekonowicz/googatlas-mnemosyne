0df5f3be9276f4eb2cffe56ebc3775a86b55b8d0
jarekko <jaroslaw.kopec@student.uw.edu.pl>
2017-03-19 15:49:12 +0100

    added scale and limiter to url

--- starter.py
+++ starter.py
@@ -22,26 +22,40 @@ def get_img_by_src(soup):
             img_list.append(img.get("src"))
     return img_list
 
-def get_img_by_File(soup):
+# def get_img_from_img_page(soup):
+#     return 1
+
+def get_img_by_File(soup, scale, limit):
     img_list = []
     img_dict = {}
     n = 0
     for link in soup.find_all('a', href=True):
         if '/wiki/' in link['href'] and 'File:' in link['href'] and '.jpg' in link['href']:
-            img_list.append(link['href'])
-            n += 1
-            img_dict[n] = link['href']
+            if link['href'] in img_list:
+                continue
+            else:
+
+                img_page_address = 'http://commons.wikimedia.org' + link['href']
+                img_list.append(link['href'])
+                img_page = get_page(img_page_address)
+                img_page_soup = BeautifulSoup(img_page)
+                miniature = get_img_by_src(img_page_soup)
+                n += 1
+                img_dict[miniature[scale]] = img_page_address
+                # liczba powyzej pozwala regulowac wielkosc grafik
+                if n >= limit:
+                    break
         else:
             continue
     return img_dict
 
-@app.route("/<query>")
-def hello(query):
+@app.route("/<query>/<scale>/<limit>")
+def scrape_and_list(query, scale, limit):
     cap_list = []
     address = get_address(query)
     content = get_page(address)
     content = BeautifulSoup(content)
-    results = get_img_by_File(content)
+    results = get_img_by_File(content, int(scale), int(limit))
     return render_template("search.html", results=results)
 
 if __name__ == "__main__":
